87b99ba866f2d92e58471d835245d622082c6d91
Aditya Gupta <adityag.ug19.cs@nitp.ac.in>
2021-04-07 02:28:33 +0530

    Added one2one, updated name matching

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ const result = document.getElementById("search-result");
 result.innerHTML = `<p>Type in something first...</p>`
 
 const xobj = new XMLHttpRequest()
+let pokedex = []
 xobj.overrideMimeType('application/json')
-xobj.open('GET', 'pokedex.json', true)  // 3rd param is "async: true"
+xobj.open('GET', 'one2one.json', true)  // 3rd param is "async: true"
 xobj.onreadystatechange = _ => {
     if( xobj.readyState == 4 && xobj.status == "200" ) {
         // .open won't return a value, just returns undefined
 
-        console.log(xobj.responseText)
+        for (p of JSON.parse(xobj.responseText)) {
+            pokedex.push(p['name'])
+        }
     }
 };
 xobj.send(null)
@@ -26,7 +29,24 @@ xobj.send(null)
 //         console.error(err)
 //     }) 
 
-function searchPokemon(element) {
-    console.log("Called")
-    console.log(element);
+function searchPokemon(input) {
+    if(input.value === '')  return;
+
+    let regex_name = '';
+    for (c of input.value) {
+        regex_name.concat('\\w*')
+    }
+    if(regex_name.length > 0) {
+        regex_name = regex_name.substr(0, regex_name.length-3)
+    }
+
+    const poke_regex = RegExp(regex_name, 'i')
+    const possibilities = []
+    for (p of pokedex) {
+        if(poke_regex.test(p)) {
+            possibilities.push(p)
+        }
+    }
+    
+    console.log("Possibilities: ", p)
 }
