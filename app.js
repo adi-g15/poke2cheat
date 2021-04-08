6a99e7e28182d083fc2abddc69392a4dff010c05
Aditya Gupta <adityag.ug19.cs@nitp.ac.in>
2021-04-08 12:06:32 +0530

    update behaviour when empty

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ let pokedex = []
 xobj.overrideMimeType('application/json')
 xobj.open('GET', 'one2one.json', true)  // 3rd param is "async: true"
 xobj.onreadystatechange = _ => {
-    if( xobj.readyState == 4 && xobj.status == "200" ) {
+    if (xobj.readyState == 4 && xobj.status == "200") {
         // .open won't return a value, just returns undefined
 
         pokedex = JSON.parse(xobj.responseText)
@@ -34,44 +34,69 @@ function changeMaxResults(number) {
 }
 
 function searchPokemon(input) {
-    if(input.value === '')  return;
-
-    let regex_name = '\\w*';
-    for (c of input.value) {
-        if(c === '_')
-            regex_name = regex_name.concat('\\w')
-        else
-            regex_name = regex_name.concat(c)
+    if (input.value === '') {
+        result.innerHTML = `
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Pokemon</th>
+                        <th>Link</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    ${pokedex.slice(0, MAX_RESULTS).map(p => `
+                    <tr>
+                        <td>${p.id}</td>
+                        <td>${p.name}</td>
+                        <td>
+                            <a href=${`https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/images/${p.id}.png`}>
+                                <img src=${`https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/sprites/${p.id}MS.png`} />
+                            </a>
+                        </td>
+                    </tr>`).join('')}
+                </tbody>
+            </table>
+            `
+    } else {
+        let regex_name = '\\w*';
+        for (c of input.value) {
+            if (c === '_')
+                regex_name = regex_name.concat('\\w')
+            else
+                regex_name = regex_name.concat(c)
             regex_name = regex_name.concat('\\w*')
+        }
+
+        const poke_regex = RegExp(regex_name, 'i')
+        const possibilities = pokedex.filter(({ name }) => poke_regex.test(name))
+            .slice(0, MAX_RESULTS);
+
+        result.innerHTML = `
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Pokemon</th>
+                        <th>Link</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    ${possibilities.map(p => `
+                    <tr>
+                        <td>${p.id}</td>
+                        <td>${p.name}</td>
+                        <td>
+                            <a href=${`https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/images/${p.id}.png`}>
+                                <img src=${`https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/sprites/${p.id}MS.png`} />
+                            </a>
+                        </td>
+                    </tr>`).join('')}
+                </tbody>
+            </table>
+            `
+
+        console.debug(`Search: ${input.value}\nRegex Expression: ${regex_name}`)
     }
 
-    const poke_regex = RegExp(regex_name, 'i')
-    const possibilities = pokedex.filter(({name}) => poke_regex.test(name))
-                                 .slice(0, MAX_RESULTS);
-
-    result.innerHTML = `
-        <table>
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Pokemon</th>
-                    <th>Link</th>
-                </tr>
-            </thead>
-            <tbody>
-                ${possibilities.map(p => `
-                <tr>
-                    <td>${p.id}</td>
-                    <td>${p.name}</td>
-                    <td>
-                        <a href=${`https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/images/${p.id}.png`}>
-                            <img src=${`https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/sprites/${p.id}MS.png`} />
-                        </a>
-                    </td>
-                </tr>`).join('')}
-            </tbody>
-        </table>
-        `
-
-    console.debug(`Search: ${input.value}\nRegex Expression: ${regex_name}`)
 }
